750ad6b614a93da293a703eee11c31f417574598
Eldur-Grooze <39276817+Eldur-Grooze@users.noreply.github.com>
2018-12-19 16:07:29 -0700

    minor changes for convenience

--- sudoku.sh
+++ sudoku.sh
@@ -11,13 +11,12 @@ if [[ $1 == "h" ]]; then
   printf "values that are not default, the rest is assumed empty.\n\n"
   printf "In case of absence of file's name, the option will be given to entry values through your console.\n\n"
   printf "5 test files are provided for testing this small programm.\n"
-  printf "sudokuTest.txt - typical Sudoku puzzle to solve.\n"
   printf "twoInBoxRow.txt - Sudoku puzzle with repeating values in the same row and box, which is against rules.\n"
   printf "empty.txt - empty file with no entries in it.\n"
-  printf "solved.txt - file with already complete Sudoku.\n"
+  printf "solved.txt - file with invalid Sudoku board.\n"
   printf "letter.txt - typical Sudoku puzzle, but with some letters insted of numbers\n"
   printf "unsolvable.txt - Sudoku puzzle with no possible solution\n\n"
+  printf "sudokuTest.txt - sudokuTest10.txt - Sudoku puzzles to solve.\n"
 else
-  printf "\nLaunching java programm. Executing java Main $1\n\n"
-  java Main $1
+  for f in *.txt ; do echo "solving $f"; java Main $f && echo "Success!"; done
 fi
